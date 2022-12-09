95846e046cc7015b0225f3f680778946c5c65b24
Petertbdot <ojooluwatobiloba@gmail.com>
2022-12-08 16:55:30 -0800

    1-compiler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point 
+ *
+ * Return: Always 0 (sucess)
+ */
+int main (void)
+{
+	printf ("Size of a char: %lu byte(s)\n", sizeof(char)));
+	printf ("Size of an int: %lu byte(s)\n, six=zeof(int));
+	printf ("Size of a long 
