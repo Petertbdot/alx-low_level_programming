9ac7ae6cad7f3c5e9d0f7305537a99f9cc32ba81
Petertbdot <ojooluwatobiloba@gmail.com>
2022-12-08 21:16:44 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stido.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- *  main - print exactly and that piece of art is useful
- *
- *  Return: Always 1 (Sucess)
- */
+* main - Entry Point
+*
+* Return: Always 1 (Sucess)
+*/
 int main(void)
-
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
